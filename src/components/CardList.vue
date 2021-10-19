ebd7d8b40fb228c80322aa6ca13a23c0e4c50f47
MADRID\adolfoisrael.morenob <adolfoisrael.morenobarbero@unir.net>
2021-10-19 17:50:22 +0200

    clase 144

---
+++ src/components/CardList.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="row">
+  <div class="col-12">
+{{paises}}
+  </div>
+</div>
+
+</template>
+
+<script>
+import {computed, onMounted} from 'vue'
+import {useStore} from 'vuex'
+import {store} from "core-js/internals/reflect-metadata";
+export default {
+  setup(){
+    const store = useStore()
+    const paises = computed(()=>{
+      return store.getters.topPaisesPoblacion
+    })
+    onMounted(()=>{
+      store.dispatch('getPaises')
+    })
+    return {paises}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,39 @@
 import { createStore } from 'vuex'
+import {computed} from "vue";
+import {store} from "core-js/internals/reflect-metadata";
 
 export default createStore({
   state: {
+    paises:[],
+    paisesFiltrados:[]
   },
   mutations: {
+    setPaises(state,payload){
+      state.paises = payload
+    },
+    setPaisesFiltrados(state,payload){
+      state.paises = payload
+    }
   },
   actions: {
+    async getPaises({commit}){
+      try{
+        const res = await fetch('api.json')
+        const data = await res.json()
+        commit('setPaises',data)
+      }catch (e) {
+        console.log(e)
+      }
+    }
+  },
+  getters:{
+
+  topPaisesPoblacion(state){
+    return state.paises.sort((a,b)=>
+       a.population<b.population?1:-1
+    )
+  }
+
   },
   modules: {
   }
